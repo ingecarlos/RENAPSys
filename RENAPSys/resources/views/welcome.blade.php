d319e7c17d0d3592c6c519a2e248808888f1bb71
Carlos Aguilar <carlos.ricardo.crac@gmail.com>
2019-12-13 07:49:22 -0600

    Update welcome.blade.php

--- RENAPSys/resources/views/welcome.blade.php
+++ RENAPSys/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    RENAPSys
+                    RENAPSys 2019
                 </div>
 
                 <div class="links">
