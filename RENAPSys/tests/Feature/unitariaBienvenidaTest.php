2b650f035546e9df9e755cf36fad33569a79434e
lnmrcloud <39037959+lnmrcloud@users.noreply.github.com>
2019-12-13 19:23:52 -0600

    prueba unitaria de Bienvenida

---
+++ RENAPSys/tests/Feature/unitariaBienvenidaTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class unitariaBienvenidaTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUnitariaBienvenida()
+    {
+
+        /*
+            Prueba unitaria para la verificacion del estado de pagina
+            inicial y de mensaje Bienvenida del Sitio
+        */
+        
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        
+        $response->assertSeeText('RENAPSys 2019');
+
+    }
+}
