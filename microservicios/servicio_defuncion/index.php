a2cf4ac256081ab721b421e078d96f7fd1d7234f
lnmrcloud <39037959+lnmrcloud@users.noreply.github.com>
2019-12-21 07:32:23 -0600

    pruebas servicios defuncion
    servicio defuncion
    conexion bd - cadena de conexion

--- microservicios/servicio_defuncion/index.php
+++ microservicios/servicio_defuncion/index.php
@@ -4,13 +4,14 @@ include('library/template.php');
 //$today = date("d-m-Y"); 
 //Servicio de Defuncion - setDefuncion 
 
+
 	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 			if( !empty($_POST['dpi']) && !empty($_POST['fecha'])){
 			$dpi =  $_POST['dpi'];
 			$fecha =  $_POST['fecha'];
 
 			//Verificar si ya existe una defuncion con el dpi solicitado
-			$sql = $pdo->prepare("SELECT persona.id_persona FROM defuncion, persona WHERE persona.dpi =:dpi and persona.id_persona=defuncion.persona_id_persona");
+			$sql = $pdo->prepare("SELECT persona.id_persona FROM Defuncion, Persona WHERE persona.dpi =:dpi and Persona.id_persona=Defuncion.persona_id_persona");
 			$sql->bindParam(':dpi', $_POST['dpi']);
 			$sql->execute();
 
@@ -18,14 +19,15 @@ include('library/template.php');
 		    if(json_encode($sql->fetch(PDO::FETCH_ASSOC)) == "false"){
 		    		//echo $dpi;
 		    		// obtener el id_persona de la persona
-		    		$sql2 = $pdo->prepare("SELECT id_persona FROM persona WHERE DPI='$dpi'");
+		    		$sql2 = $pdo->prepare("SELECT id_persona FROM Persona WHERE DPI=$dpi");
 				    $sql2->execute();
 				    $result = $sql2->fetch(PDO::FETCH_ASSOC);
 				    $id_persona = $result['id_persona'];
 				    //print($id_persona);
 				    // id_persona obtenido -> $id_persona
-				    $sql3 = $pdo->prepare("INSERT INTO defuncion(persona_id_persona, fecha) VALUES($id_persona, '$fecha')");
+				    $sql3 = $pdo->prepare("INSERT INTO Defuncion(Persona_id_persona, fecha) VALUES($id_persona, '$fecha');");
 				    $sql3->execute();
+
 				    exit();
 			//el dpi solicitado si posee una defuncion asignada -> responder error codigo 500
 		    }else{
@@ -41,7 +43,7 @@ include('library/template.php');
 
 				$dpi =  $_GET['dpi'];
 				//Verificar si ya existe una defuncion con el dpi solicitado
-				$sql = $pdo->prepare("SELECT d.id_defuncion, d.fecha, p.Nombre , p.Apellido FROM defuncion as d, persona as p WHERE p.DPI =:dpi and d.Persona_id_persona  = p.id_persona;");
+				$sql = $pdo->prepare("SELECT d.id_defuncion, d.fecha, p.Nombre , p.Apellido FROM Defuncion as d, Persona as p WHERE p.DPI =:dpi and d.Persona_id_persona  = p.id_persona;");
 				$sql->bindParam(':dpi', $_GET['dpi']);
 				$sql->execute();
 
--- microservicios/servicio_defuncion/library/config.php
+++ microservicios/servicio_defuncion/library/config.php
@@ -1,17 +1,15 @@
 <?php
 $hote = '35.202.248.44';
 $port = "3306";
-$nom_bdd = 'renapdb';
+$nom_bdd = 'RENAPDB';
 $utilisateur = 'root';
 $mot_de_passe ='123';
 
 try {
 	//On test la connexion à la base de donnée
-    $pdo = new PDO('mysql:host='.$hote.';port='.$port.';dbname='.$nom_bdd, $utilisateur, $mot_de_passe);
-    echo 'conexión lograda';
+    $pdo = new PDO('mysql:host='.$hote.':'.$port.';dbname='.$nom_bdd, $utilisateur, $mot_de_passe);
 
 } catch(Exception $e) {
-	echo 'error en la conexión';
 	//Si la connexion n'est pas établie, on stop le chargement de la page.
 	reponse_json($success, $data, 'Echec de la connexion à la base de données');
     exit();
